c3ce9f43de3854a25df3b8be64f9996bae8ef14b
Ha Nguyen <hnguyen68@academic.rrc.ca>
2019-10-08 08:50:54 -0500

    created Cuisine View, Model, Controller

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class CuisinesController < ApplicationController
-  def show; end
+  def index
+    @pagy, @cuisines = pagy(Cuisine.all, items: 10)
+  end
+
+  def show
+    @cuisine = Cuisine.find(params[:id])
+    @cuisine.collection_restaurants
+  end
+
+  def cuisine_params
+    params.require(:cuisine).permit(:id, :name)
+  end
 end
